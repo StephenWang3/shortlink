2cfd8e821ee4d7b05e0e489c5fe241eb919986dc
Stephen Wang <stephenwzz77@gmail.com>
2023-12-22 21:57:04 +0800

    feature: 短链接后管系统用户修改信息功能

--- admin/src/main/java/com/nageoffer/shortlink/admin/service/UserService.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/UserService.java
@@ -3,7 +3,9 @@ package com.nageoffer.shortlink.admin.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.nageoffer.shortlink.admin.dao.entity.UserDO;
 import com.nageoffer.shortlink.admin.dto.req.UserRegisterReqDTO;
+import com.nageoffer.shortlink.admin.dto.req.UserUpdateReqDTO;
 import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
+import org.springframework.web.bind.annotation.RequestBody;
 
 /**
  * 用户接口层
@@ -29,5 +31,11 @@ public interface UserService extends IService<UserDO> {
      * @param requestParam 注册用户信息
      */
     void register(UserRegisterReqDTO requestParam);
+
+    /**
+     * 根据用户名修改用户信息
+     * @param requestParam 修改用户请求参数
+     */
+    void update(@RequestBody UserUpdateReqDTO requestParam);
 }
 
