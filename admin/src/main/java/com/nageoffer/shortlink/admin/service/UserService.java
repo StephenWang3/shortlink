692d7965b5254b121fee88f6adb62df9537ada7b
Stephen Wang <stephenwzz77@gmail.com>
2023-12-23 19:45:11 +0800

    feature: 短链接后管系统用户登录退出相关功能

--- admin/src/main/java/com/nageoffer/shortlink/admin/service/UserService.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/UserService.java
@@ -2,10 +2,11 @@ package com.nageoffer.shortlink.admin.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.nageoffer.shortlink.admin.dao.entity.UserDO;
+import com.nageoffer.shortlink.admin.dto.req.UserLoginReqDTO;
 import com.nageoffer.shortlink.admin.dto.req.UserRegisterReqDTO;
 import com.nageoffer.shortlink.admin.dto.req.UserUpdateReqDTO;
+import com.nageoffer.shortlink.admin.dto.resp.UserLoginRespDTO;
 import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
-import org.springframework.web.bind.annotation.RequestBody;
 
 /**
  * 用户接口层
@@ -36,6 +37,28 @@ public interface UserService extends IService<UserDO> {
      * 根据用户名修改用户信息
      * @param requestParam 修改用户请求参数
      */
-    void update(@RequestBody UserUpdateReqDTO requestParam);
+    void update(UserUpdateReqDTO requestParam);
+
+    /**
+     * 用户登录
+     * @param requestParam 登录用户请求参数
+     * @return  登录用户返回信息
+     */
+    UserLoginRespDTO login(UserLoginReqDTO requestParam);
+
+    /**
+     * 检查用户是否登录
+     * @param username 用户名
+     * @param token 用户登录凭证
+     * @return 用户已登录返回True, 未登录返回False
+     */
+    Boolean checkLogin(String username, String token);
+
+    /**
+     * 用户退出登录
+     * @param username 用户名
+     * @param token 用户登录凭证
+     */
+    void logout(String username, String token);
 }
 
