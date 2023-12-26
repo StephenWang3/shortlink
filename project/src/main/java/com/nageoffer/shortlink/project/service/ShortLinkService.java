daf69f7c0498986f7a9c9cf60256b8d973f431bf
Stephen Wang <stephenwzz77@gmail.com>
2023-12-26 20:57:22 +0800

    feature: 开发短链接分页查询功能

--- project/src/main/java/com/nageoffer/shortlink/project/service/ShortLinkService.java
+++ project/src/main/java/com/nageoffer/shortlink/project/service/ShortLinkService.java
@@ -1,9 +1,12 @@
 package com.nageoffer.shortlink.project.service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.nageoffer.shortlink.project.dao.entity.ShortLinkDO;
 import com.nageoffer.shortlink.project.dto.req.ShortLinkCreateReqDTO;
+import com.nageoffer.shortlink.project.dto.req.ShortLinkPageReqDTO;
 import com.nageoffer.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
+import com.nageoffer.shortlink.project.dto.resp.ShortLinkPageRespDTO;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,5 +20,12 @@ public interface ShortLinkService extends IService<ShortLinkDO> {
      * @param requestParam 创建短链接请求参数
      * @return 短链接创建信息
      */
-    public ShortLinkCreateRespDTO createShortLink(ShortLinkCreateReqDTO requestParam);
+    ShortLinkCreateRespDTO createShortLink(ShortLinkCreateReqDTO requestParam);
+
+    /**
+     * 短链接分页查询
+     * @param requestParam 短链接分页查询请求参数
+     * @return 短链接分页查询结果
+     */
+    IPage<ShortLinkPageRespDTO> pageShortLink(ShortLinkPageReqDTO requestParam);
 }
